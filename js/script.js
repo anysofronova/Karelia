b3c02e55ebfa141a6f84f81ce5a1cfcde22aa531
Anna <anysofronova@yandex.ru>
2021-12-28 18:11:29 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-$(function(){
+$(function() {
     $('.tours__sliders').slick({
-        dots:false,
+        dots: false,
         autoplay: true,
         autoplaySpeed: 4000,
         infinite: true,
@@ -11,29 +11,31 @@ $(function(){
 })
 
 const navMenu = document.getElementById('nav-menu'),
-	navToggle = document.getElementById('nav-toggle'),
-	navClose = document.getElementById('nav-close');
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
 
 // ===================== Show menu =====================
 if (navToggle) {
-	navToggle.addEventListener('click', () => {
-		navMenu.classList.add('show-menu');
-	})
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    })
 }
 
 // ===================== Close menu =====================
 if (navClose) {
-	navClose.addEventListener('click', () => {
-		navMenu.classList.remove('show-menu');
-	})
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    })
 }
+
+
 
 // ===================== Remove menu =====================
 const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction() {
-	const navMenu = document.getElementById('nav-menu');
-	navMenu.classList.remove('show-menu');
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show-menu');
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
@@ -41,13 +43,13 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 let toggle = document.getElementById('nav-toggle');
 window.addEventListener('scroll', function(e) {
     let last_known_scroll_position = window.scrollY;
-    if(last_known_scroll_position > 730){
+    if (last_known_scroll_position > 730) {
         toggle.style.color = 'black';
-    }else{
+    } else {
         toggle.style.color = 'white';
     }
 });
-  
+
 $('a[href^="#"').on('click', function() {
 
     let href = $(this).attr('href');
